e6a0ec955725407a1b5d234fdbad60f40730f335
Ryne <ryne@enclave.vn>
2017-06-22 08:55:10 +0700

    add method test

--- app/src/main/java/com/example/ryne/myapplication/Activity/PersonalInfor/PersonalInfoActivity.java
+++ app/src/main/java/com/example/ryne/myapplication/Activity/PersonalInfor/PersonalInfoActivity.java
@@ -1,8 +1,14 @@
 package com.example.ryne.myapplication.Activity.PersonalInfor;
 
+import android.widget.Toast;
+
 /**
  * Created by ryne on 14/06/2017.
  */
 
 public class PersonalInfoActivity {
+
+    public void test(){
+    }
+
 }
